732d6bb7cc02387838401bbe5b743e73ba06a288
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-26 22:50:19 +0100

    fix

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -6,9 +6,5 @@ function getCurrentYear() {
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {};
 
-  [`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
   return budget;
 }
