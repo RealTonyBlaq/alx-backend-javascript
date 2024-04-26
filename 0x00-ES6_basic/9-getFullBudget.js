53271299d96d5ef3c1ca3b28f60a3d559763a373
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-26 23:19:30 +0100

    rewriting to use ES6 method props

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -5,8 +5,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const fullBudget = {
     ...budget,
     getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: function (income) => `${income} euros`,
-    },
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
