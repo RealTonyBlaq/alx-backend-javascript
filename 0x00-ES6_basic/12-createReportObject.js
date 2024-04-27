142fe1a3d97de54e1c499083856d08f891b7319e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-27 08:01:39 +0100

    create Report

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,3 @@
 export default function createReportObject(employeesList) {
-  return { allEmployees: employeesList, getNumberOfDepartments: employeesList.l}
+  return { allEmployees: employeesList, getNumberOfDepartments: employeesList.length };
 }
