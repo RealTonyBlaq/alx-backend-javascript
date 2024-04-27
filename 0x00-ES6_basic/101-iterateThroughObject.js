db867f9fe36e7bbc55915be955f1033c490ef086
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-27 08:40:13 +0100

    iterate through obj

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let format = '';
-  let count = 0;
-  for (const employee of reportWithIterator) {
-    format += employee;
-    if (count !== reportWithIterator.length - 1) {
-      format += ' | ';
-    }
-    count++;
-  }
-
+  const employees = [...reportWithIterator];
+  const format = employees.join(' | ');
   return format;
 }
