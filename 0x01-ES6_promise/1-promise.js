44d3abb80f1365e75139d216b1a866a5717168b3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 19:23:57 +0100

    fulfill if true, else reject

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,6 @@
 export default getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
-    if (success) resolve({status: 200, })
+    if (success) resolve({status: 200, body: 'Success'})
+    else reject()
   });
 }
