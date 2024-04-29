74014ec1848144efc6772f5a44908cfb7c89c42d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 22:37:33 +0100

    await

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  if (firstName && lastName )
+  if (firstName && lastName && filename) {
+    
+  }
 }
