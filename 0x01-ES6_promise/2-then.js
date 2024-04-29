1a62ca39f2cf43414f6d54783fc2838e82a4aa78
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 19:58:04 +0100

    append promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return ({ status: 200, body: 'Success' });
-    }, () => Error())
-    .then(() => {
-      console.log('Got a response from the API');
-      return ({ status: 200, body: 'Success' });
-    }, () => Error())
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return ({ status: 200, body: 'Success' });
