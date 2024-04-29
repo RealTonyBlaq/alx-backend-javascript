6610fd2761a93154eceea90f0f5d6d3f955f109e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 23:02:11 +0100

    await

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,20 @@ async function handleProfileSignup(firstName, lastName, filename) {
   if (firstName && lastName && filename) {
     const signUp = await signUpUser(firstName, lastName);
     const upload = await uploadPhoto(filename);
+
+    const [signUp, upload] = await Promise.allSettled([signUpPromise, uploadPromise]);
+    
+    const signUpResult = {
+      status: signUp.status,
+      value: signUp.status === 'fulfilled' ? signUp.value : null
+    };
+    
+    const uploadResult = {
+      status: upload.status,
+      value: upload.status === 'fulfilled' ? upload.value : null
+    };
+
+    return [signUpResult, uploadResult];
     return ([{ status, signUp }, { status, upload }]);
   }
   throw new Error();
