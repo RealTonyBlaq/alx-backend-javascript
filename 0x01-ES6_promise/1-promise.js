659fba18aa67819a23e01cde3b4def9be0059c71
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 19:29:57 +0100

    fulfill if true, else reject

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-export default getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success) resolve({status: 200, body: 'Success'});
     else reject(Error('The fake API is not working currently'));
