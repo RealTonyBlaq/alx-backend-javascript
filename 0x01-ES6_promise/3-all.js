b055a398d210132968bd3e11bba68a500e10e0be
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 20:47:10 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  const upload = uploadPhoto()
+  const upload = uploadPhoto();
 }
