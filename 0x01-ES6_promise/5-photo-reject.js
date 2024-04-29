a3a8aa897aeb7d463a0b0a7dc8d88e141179299c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-29 21:35:54 +0100

    reject promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -2,5 +2,5 @@ export default function uploadPhoto(filename) {
   if (filename instanceof String) {
     return Promise.reject(new Error(`${filename} cannot be processed`));
   }
-  
+  throw Error();
 }
