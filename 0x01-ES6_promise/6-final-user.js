72307772bd3b0ded62a8232d50ec320a45ffc09f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-30 21:04:29 +0100

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,28 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-async function handleProfileSignup(firstName, lastName, filename) {
+function handleProfileSignup(firstName, lastName, filename) {
   if (firstName && lastName && filename) {
     const signUpPromise = signUpUser(firstName, lastName);
     const uploadPromise = uploadPhoto(filename);
 
-    const [signUp, upload] = await Promise.allSettled([signUpPromise, uploadPromise])
+    return Promise.allSettled([signUpPromise, uploadPromise])
+      .then((values) => values.map((result) => ({
+        status: result.status,
+        value: result.value === 'fulfilled' ? result.value : result.reason.message,
+      })))
       .catch((error) => {
         console.error(new Error('Error signing up the User'), error);
         return ([]);
       });
-
-    const signUpResult = {
-      status: signUp.status,
-      value: signUp.status === 'fulfilled' ? signUp.value : signUp.reason.message,
-    };
-
-    const uploadResult = {
-      status: upload.status,
-      value: upload.status === 'fulfilled' ? upload.value : upload.reason.message,
-    };
-
-    return [signUpResult, uploadResult];
   }
   throw new Error('Incomplete parameters');
 }
