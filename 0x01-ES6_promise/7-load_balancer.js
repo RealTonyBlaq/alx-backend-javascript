4744d303e063fd62658d663db881628d8ba317bf
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-30 21:29:39 +0100

    promise balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.any(chinaDownload, USDownload)
   .then((result) => result)
-  .catch()
+  .catch(() => console.log)
 }
