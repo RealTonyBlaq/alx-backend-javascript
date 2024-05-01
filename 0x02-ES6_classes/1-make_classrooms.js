0f65dd69ca672b00e4a762a37ba7616bd0da2af1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-01 21:59:33 +0100

    making classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,5 +3,5 @@ import ClassRoom from "./0-classroom";
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
   sizes.forEach((maxStudentsSize) => ClassRoom(maxStudentsSize));
-  
+  return sizes;
 }
