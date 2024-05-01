87bb2c2361b5d3c7cb1d0b557459c6c3fb38253f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-02 00:37:58 +0100

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,9 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors === 'number') this._floors = floors;
+    else throw new TypeError('Floors must be a number');
+  }
+}
