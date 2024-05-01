4362dcdb5ace14fe0e4e538734089a3cad245a11
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-02 00:31:12 +0100

    fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,6 +2,10 @@ export default class Building {
   constructor(sqft) {
     if (typeof sqft === 'number') this._sqft = sqft;
     else throw new TypeError('Sqft must be a number');
+
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
@@ -11,9 +15,5 @@ export default class Building {
   set sqft(value) {
     if (typeof value === 'number') this._sqft = value;
     else throw new TypeError('Sqft must be a number');
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
