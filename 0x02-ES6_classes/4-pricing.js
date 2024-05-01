16ba1fd4eefdfa14c7554db90ce5a9b1ccd79945
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-01 23:24:13 +0100

    pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,13 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number') this._amount = amount;
+    else throw new TypeError('Amount must be a number');
+
+    if (currency instanceof Currency) this._currency = currency();
+    else throw new TypeError('Currency must be an instance of the class Currency');
+  }
+
+  
+}
