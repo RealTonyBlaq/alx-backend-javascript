00912fddf4a8e70de6c3990c982cfb31e9ba98b0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-03 21:25:17 +0100

    fix code

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -17,7 +17,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = {};
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
