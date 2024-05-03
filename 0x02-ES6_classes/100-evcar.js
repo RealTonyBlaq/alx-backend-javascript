e5e94a4eb508aac766cbd25111452991c1bf146c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-03 22:38:58 +0100

    fix

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,7 +6,8 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
+  /* eslint-disable class-methods-use-this */
   cloneCar() {
-    return new Car;
+    return new Car();
   }
 }
