4ab67ca28972c1ed5acd94cc66e22b42cb6a4fa4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-03 21:15:30 +0100

    fix code

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,6 +13,26 @@ export class HolbertonClass {
   }
 }
 
+export class StudentHolberton {
+  constructor(firstName, lastName) {
+      this._firstName = firstName;
+      this._lastName = lastName;
+      this._holbertonClass = holbertonClass;
+    }
+  
+    get fullName() {
+      return `${this._firstName} ${this._lastName}`;
+    }
+  
+    get holbertonClass() {
+      return this.holbertonClass;
+    }
+  
+    get fullStudentDescription() {
+      return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
+    }
+}
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -21,26 +41,6 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-export class StudentHolberton {
-  constructor(firstName, lastName) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
-  }
-
-  get fullName() {
-    return `${this._firstName} ${this._lastName}`;
-  }
-
-  get holbertonClass() {
-    return this.holbertonClass;
-  }
-
-  get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
-  }
-}
 
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
