e7780453392fa69b3c9a4a2b65b20a9333cdfae4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-03 22:05:28 +0100

    new car in a car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -7,6 +7,6 @@ export default class Car {
 
   /* creates a new instance of a Car class */
   cloneCar() {
-    return Car(this._brand, this._motor, this._color);
+    return this.Car(this._brand, this._motor, this._color);
   }
 }
