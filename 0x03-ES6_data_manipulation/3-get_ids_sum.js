6758a9200c5cb475a9ec3cfab4aca145e96bb899
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-05 13:12:46 +0100

    sum of IDs

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 /* getStudentIdsSum - Returns the sum of all the student ids. */
 export default function getStudentIdsSum(arrayOfObjects) {
-  return arrayOfObjects.reduce((acc, obj) => acc + obj.id, 0)
+  return arrayOfObjects.reduce((acc, obj) => acc + obj.id, 0);
 }
