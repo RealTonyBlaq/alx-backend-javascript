5eff0b1d3671ffcf6864684f66c435d2bbc9b982
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-05 14:13:29 +0100

    update by grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,7 +5,7 @@ import getStudentsByLocation from './2-get_students_by_loc';
 
 export default function updateStudentGradeByCity(listOfStudents, city, newGrades) {
   const students = getStudentsByLocation(listOfStudents, city);
-  const IDs = newGrades.reduce((acc, obj) => acc.push(obj.studentId), [])
+  const IDs = newGrades.reduce((acc, obj) => acc.push(obj.studentId), []);
   newGrades.forEach((updateObj) => {
     students.filter((student) => student.id === updateObj.studentId).map((obj) => {
         if (obj.id in IDs) obj.grade = updateObj.grade;
