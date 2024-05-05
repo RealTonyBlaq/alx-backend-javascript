08ab1910ba48cb92fc58a6622919461d3d6f1d5f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-05 13:28:37 +0100

    update by grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 /*
 updateStudentGradeByCity - Returns an array of students for a specific city with their new grade
 */
-export default function updateStudentGradeByCity(listOfStudents, city, grades) {
-  
+export default function updateStudentGradeByCity(listOfStudents, city, newGrades) {
+  for (const student in )
 }
