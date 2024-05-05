54d03371eb0105fc7cdb9d6601e5b840460c2eef
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-05 13:04:27 +0100

    get obj by location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,7 @@
-/* function getStudentsByLocation that returns an array of objects who are located in a specific city. */
+/*
+getStudentsByLocation - Returns an array of objects
+who are located in a specific city.
+*/
 export default function getStudentsByLocation(arrayOfObjects, city) {
   return arrayOfObjects.filter((obj) => obj.location === city);
 }
