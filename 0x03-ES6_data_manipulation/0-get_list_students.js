ae6577e295e2d5284a430bc4261eaf5504045d67
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-05 12:31:14 +0100

    get students list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,7 @@
 export default function getListStudents() {
   const st1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const st2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const st3 = { id: 5, firstName: 'Guillaume', location: 'San Francisco' };
+  const st3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  return 
 }
