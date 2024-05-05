9854a95d1b6fbf8fabeaefa139e5e4990165dce2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-05 12:47:50 +0100

    get IDs

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,8 @@
 export default function getListStudentIds(arrayOfObjects) {
   const IDs = [];
-  if (Array.isArray(arrayOfObjects))
+  if (Array.isArray(arrayOfObjects)) {
+    
+  }
   for (const obj of arrayOfObjects) {
     IDs.push(obj.id);
   }
