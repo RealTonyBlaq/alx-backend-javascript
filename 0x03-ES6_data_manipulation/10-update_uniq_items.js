20b8de56d1d14297fa2aca2710c2121bc88e0223
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-06 09:10:26 +0100

    update map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+/*
+updateUniqueItems - Returns an updated map for all items with initial quantity at 1
+*/
+export default function updateUniqueItems(map) {
+  map.forEach((value, key) => {
+    if (value === 1) map.set(key, 100);
+  });
+  return map;
+}
