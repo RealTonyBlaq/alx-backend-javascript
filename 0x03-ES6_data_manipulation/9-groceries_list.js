65176933e18ade3433b02f1ec64a5cb2fab6f8d6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-06 08:15:25 +0100

    list of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+/* groceriesList - Returns a map of groceries with the following items (name, quantity) */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
