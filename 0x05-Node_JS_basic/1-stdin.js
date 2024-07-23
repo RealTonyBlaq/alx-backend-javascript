b4c53ff8b2f2e88f6e23048e6bcd182ccf96a3fa
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 20:12:37 +0100

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
-  const myName = process.stdin.read().split('\n')[0];
-  if (myName) console.log(`Your name is: ${myName}`);
+  const myName = process.stdin.read();
+  if (myName) process.stdout.write(`Your name is: ${myName}`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit(0)
+  process.exit(0);
 });
