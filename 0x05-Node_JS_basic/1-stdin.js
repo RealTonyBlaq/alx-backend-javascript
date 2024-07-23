62bf871be3f60d45ffee9acb83250289e6ae7758
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 19:54:22 +0100

    program

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', function (input) {
   console.log(`Your name is: ${input}`);
+  process.exit(0);
 });
 process.on('SIGTERM', () => {
   console.log('This important software is now closing');
