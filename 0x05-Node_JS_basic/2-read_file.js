6ea0487302409a3211c09560c09f64186426ce23
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 21:36:44 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,9 +5,10 @@ function countStudents(path) {
     try {
       const contents = reader.readFileSync(path, 'utf-8');
       const arrayBuffer = contents.split('\n').slice(1, -1);
+      console.log(`Number of students: NUMBER_OF_STUDENTS`)
     for (const info of arrayBuffer) {
       const studentDetail = info.split(',');
-      
+      if 
     }
     } catch (err) {
         throw Error('Cannot load the database');
