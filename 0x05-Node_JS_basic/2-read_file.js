df0b509b484690e7643e101d8916b62c528b5fa2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 21:34:30 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,15 +3,14 @@ const reader = require('node:fs');
 function countStudents(path) {
   if (path) {
     try {
-        const contents = reader.readFileSync(path, 'utf-8');
-    } catch (err) {
-        throw Error('Cannot load the database');
-    }
-    const arrayBuffer = contents.split('\n').slice(1, -1);
+      const contents = reader.readFileSync(path, 'utf-8');
+      const arrayBuffer = contents.split('\n').slice(1, -1);
     for (const student of arrayBuffer) {
       const 
     }
-    console.log(arrayBuffer);
+    } catch (err) {
+        throw Error('Cannot load the database');
+    }
   }
 }
 
