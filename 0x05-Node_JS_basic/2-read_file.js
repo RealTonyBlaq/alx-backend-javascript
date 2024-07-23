e876f9000f378b743e629966d0259f2023b6e5ee
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 22:29:09 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,7 +9,22 @@ function countStudents(path) {
       throw Error('Cannot load the database');
     }
 
-    
+    const arrayBuffer = contents.split('\n').slice(1, -1);
+    console.log(`Number of students: ${arrayBuffer.length}`);
+
+    const sortedData = {};
+    arrayBuffer.forEach((value) => {
+      const info = value.split(',');
+      const key = info[info.length - 1];
+      if (!(key in sortedData)) {
+        sortedData[key] = [];
+        }
+        sortedData[key].push(info[0]);
+      });
+
+      Object.keys(sortedData).forEach((key) => {
+        console.log(`Number of students in ${key}: ${sortedData[key].length}. List: ${sortedData[key].join(', ')}`);
+      });
   }
 }
 
