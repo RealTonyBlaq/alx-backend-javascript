6e3aaceb37b8e1fa74ae0e7ee58d5736813fdd36
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 21:04:03 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,9 @@
-import { readFile } from 'node:fs';
+import { read } from 'node:fs';
 function countStudents(path) {
   if (path) {
     readFile(path, (err, data) => {
       if (err) throw Error('Cannot load the database');
-      
+
     });
   } else {
     throw Error('Cannot load the database');
