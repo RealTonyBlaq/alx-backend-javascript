5bfbf5b3715d3dc2d855ad554bc0b47562619dcf
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 21:22:21 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,8 @@ const reader = require('node:fs');
 function countStudents(path) {
   if (path) {
     const contents = reader.readFileSync(path, 'utf-8');
-    const arrayBuffer = contents.split('\n');
+    const arrayBuffer = contents.split('\n').;
+    console.log(arrayBuffer);
   }
 }
 
