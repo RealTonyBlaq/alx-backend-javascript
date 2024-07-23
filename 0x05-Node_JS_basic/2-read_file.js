b0ca1cf3d7b37037bd20dbe9830d5ac89741073c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 21:18:01 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,8 +4,7 @@ function countStudents(path) {
   if (path) {
     reader.readFileSync(path, 'utf-8', (err, data) => {
       if (err) throw Error('Cannot load the database');
-      const firstIndex = data.indexOf('\n');
-      console.log(data.slice(firstIndex, -1));
+      console.log(data);
     });
   }
 }
