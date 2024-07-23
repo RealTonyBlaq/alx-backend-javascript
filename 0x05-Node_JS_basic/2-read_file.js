e2bd9382f647d87ee534e748a1030175a630c945
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-23 21:54:08 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,7 +5,7 @@ function countStudents(path) {
     try {
       const contents = reader.readFileSync(path, 'utf-8');
       const arrayBuffer = contents.split('\n').slice(1, -2);
-      console.log(`Number of students: ${arrayBuffer.length}`)
+      console.log(`Number of students: ${arrayBuffer.length}`);
       const sortedData = {}
     for (const info of arrayBuffer) {
       const studentDetail = info.split(',');
