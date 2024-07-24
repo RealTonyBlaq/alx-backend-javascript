9ebeec2b57535248d03db338de277458504380fc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-24 21:28:43 +0100

    async reading

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,7 +3,7 @@ const reader = require('fs');
 function countStudents(path) {
   if (path) {
     reader.readFile(path, 'utf-8', (err, contents) => {
-      if (err) throw new 
+      if (err) throw new Error('Cannot load the database');
     });
 
     const arrayBuffer = contents.split('\n').slice(1);
