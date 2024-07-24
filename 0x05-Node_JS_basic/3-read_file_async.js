63d75eed74bc8e11e2e0a540ef25db866a7c56ba
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-24 21:20:44 +0100

    async reading

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const reader = require('fs');
+
+function countStudents(path) {
+  if (path) {
+    let contents;
+    try {
+      contents = reader.readFileSync(path, 'utf-8');
+    } catch (err) {
+      throw new Error('Cannot load the database');
+    }
+
+    const arrayBuffer = contents.split('\n').slice(1);
+    let numOfStudents = 0;
+
+    arrayBuffer.forEach((value) => {
+      if (value !== '') {
+        numOfStudents += 1;
+      }
+    });
+
+    console.log(`Number of students: ${numOfStudents}`);
+
+    const sortedData = {};
+    arrayBuffer.forEach((value) => {
+      if (value !== '') {
+        const info = value.split(',');
+        const key = info[info.length - 1];
+        if (!(key in sortedData)) {
+          sortedData[key] = [];
+        }
+        sortedData[key].push(info[0]);
+      }
+    });
+
+    Object.keys(sortedData).forEach((key) => {
+      console.log(`Number of students in ${key}: ${sortedData[key].length}. List: ${sortedData[key].join(', ')}`);
+    });
+  } else {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
