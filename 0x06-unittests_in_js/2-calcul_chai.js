66319a36b8f1a2d64298c8c04857bed3b85db66a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-25 23:51:58 +0100

    function calculateNumber()

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,10 @@
+function calculateNumber(type, a, b) {
+  const d = Math.round(b);
+  const c = Math.round(a);
+  if (type === 'SUM') return (c + d);
+  if (type === 'SUBTRACT') return (d - c);
+  if (type === 'DIVIDE' && d !== 0) return (c / d);
+  return 'Error';
+}
+
+module.exports = calculateNumber;
