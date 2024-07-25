92db3e77f46ec6ab4a7bc834aaae5ac44b3539c5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-26 00:51:17 +0100

    test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -2,3 +2,7 @@ const { expect } = require('chai');
 const Utils = require('./utils');
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  
+});
