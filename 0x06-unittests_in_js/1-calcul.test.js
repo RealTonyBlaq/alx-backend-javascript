192d2b16ce648674ae2595326c2d840668a4044f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-25 23:38:42 +0100

    test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -10,6 +10,29 @@ describe('calculateNumber', () => {
     it('should return 0 when inputs are -1.4 and 1.4', () => {
       assert.strictEqual(calculateNumber('SUM', -1.4, 1.4), 0);
     });
+
+    it('should return 1 with inputs 0.5 and 0.2', () => {
+        assert.equal(calculateNumber(0.5, 0.2), 1);
+    });
+      it('should return 2 with inputs 0.5 and 0.7', () => {
+        assert.equal(calculateNumber(0.5, 0.7), 2);
+      });
+    
+      it('should return 0 with inputs 0.4 and 0.3', () => {
+        assert.equal(calculateNumber(0.4, 0.3), 0);
+      });
+    
+      it('should return 3 with inputs 1.2 and 1.7', () => {
+        assert.equal(calculateNumber(1.2, 1.7), 3);
+      });
+    
+      it('should return -3 with inputs -7.5 and 4.3', () => {
+        assert.equal(calculateNumber(-7.5, 4.3), -3);
+      });
+    
+      it('should return -5 with inputs -2 and -2.6', () => {
+        assert.equal(calculateNumber(-2, -2.6), -5);
+      });
   });
 
   describe('SUBTRACT', () => {
