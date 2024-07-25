2c56756573e931952dd2ec6b2fa750a49146f972
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-25 23:22:15 +0100

    update 0-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -4,7 +4,7 @@ function calculateNumber(type, a, b) {
   if (type === 'SUM') return (a + b);
   if (type === 'SUBTRACT') return (b - a);
   if (type === 'DIVIDE' && b !== 0) return (a / b);
-  else r
+  else return 'Error';
 }
 
 module.exports = calculateNumber;
