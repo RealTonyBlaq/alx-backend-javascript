4634370323aaa92bda94919a938f12dea847b0b4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-25 23:59:50 +0100

    fix

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ function calculateNumber(type, a, b) {
   const d = Math.round(b);
   const c = Math.round(a);
   if (type === 'SUM') return (c + d);
-  if (type === 'SUBTRACT') return (d - c);
+  if (type === 'SUBTRACT') return (c - d);
   if (type === 'DIVIDE' && d !== 0) return (c / d);
   return 'Error';
 }
