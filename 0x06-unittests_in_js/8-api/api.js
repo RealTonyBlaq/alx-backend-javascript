c931e92c2edc301ce43b8410450bb83fa10571e3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-26 01:31:27 +0100

    app

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = 
+const port = 7865;
 
 app.get('/', (req, res) => {
     res.end('Welcome to the payment system');
 });
 
-app.listen(() => {});
+app.listen((port) => {
+    
+});
