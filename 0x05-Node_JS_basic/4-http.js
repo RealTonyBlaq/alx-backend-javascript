f606466ffa616ffe8ee519e33e7957746808d7d9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-26 23:25:57 +0100

    http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,16 @@
-const { createServer } = require('http');
+const http = require('http');
 
+const hostname = '127.0.0.1';
 const port = 1245;
-const app = createServer((req, res) => {
+
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen()
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
