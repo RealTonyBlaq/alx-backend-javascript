c20f9569b88ac48a6a4fb80d12871e6949402fed
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-26 23:23:40 +0100

    http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,10 @@
 const { createServer } = require('http');
 
+const port = 1245;
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
+
+app.listen()
