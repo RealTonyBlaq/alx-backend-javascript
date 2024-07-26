22b5e5336354e90e50202704238dd43bf4a60e92
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-26 22:56:22 +0100

    fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,41 +1,42 @@
 const reader = require('fs');
 
 function countStudents(path) {
-  if (path) {
-    reader.readFile(path, 'utf-8', (err, contents) => {
-      if (err) throw new Error('Cannot load the database');
-
-      const arrayBuffer = contents.split('\n').slice(1);
-      let numOfStudents = 0;
-
-      arrayBuffer.forEach((value) => {
-      if (value !== '') {
-        numOfStudents += 1;
-      }
-      });
-
-      console.log(`Number of students: ${numOfStudents}`);
-
-      const sortedData = {};
-      arrayBuffer.forEach((value) => {
-        if (value !== '') {
-          const info = value.split(',');
-          const key = info[info.length - 1];
-          if (!(key in sortedData)) {
-            sortedData[key] = [];
+  return new Promise((resolve, reject) => {
+    if (path) {
+        reader.readFile(path, 'utf-8', (err, contents) => {
+          if (err) throw new Error('Cannot load the database');
+    
+          const arrayBuffer = contents.split('\n').slice(1);
+          let numOfStudents = 0;
+    
+          arrayBuffer.forEach((value) => {
+          if (value !== '') {
+            numOfStudents += 1;
           }
-          sortedData[key].push(info[0]);
-        }
-      });
-
-      Object.keys(sortedData).forEach((key) => {
-        console.log(`Number of students in ${key}: ${sortedData[key].length}. List: ${sortedData[key].join(', ')}`);
-      });
-    });
-    return new Promise.resolve();
-  } else {
-    throw new Error('Cannot load the database');
-  }
+          });
+    
+          console.log(`Number of students: ${numOfStudents}`);
+    
+          const sortedData = {};
+          arrayBuffer.forEach((value) => {
+            if (value !== '') {
+              const info = value.split(',');
+              const key = info[info.length - 1];
+              if (!(key in sortedData)) {
+                sortedData[key] = [];
+              }
+              sortedData[key].push(info[0]);
+            }
+          });
+    
+          Object.keys(sortedData).forEach((key) => {
+            console.log(`Number of students in ${key}: ${sortedData[key].length}. List: ${sortedData[key].join(', ')}`);
+          });
+        });
+      } else {
+        throw new Error('Cannot load the database');
+      }
+  });
 }
 
 module.exports = countStudents;
