10e727569db4d1d2e939dc76cfccde6f4511bec8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-28 15:07:43 +0100

    readDatabase

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,33 @@
+const reader = require('fs');
+
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    if (path) {
+      reader.readFile(path, 'utf-8', (err, contents) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+
+        const arrayBuffer = contents.split('\n').slice(1);
+        const sortedData = {};
+
+        arrayBuffer.forEach((value) => {
+          if (value !== '') {
+            const info = value.split(',');
+            const key = info[info.length - 1];
+            if (!(key in sortedData)) {
+              sortedData[key] = [];
+            }
+            sortedData[key].push(info[0]);
+          }
+        });
+        resolve(sortedData);
+      });
+    } else {
+      reject(new Error('Cannot load the database'));
+    }
+  });
+}
+
+module.exports = readDatabase;
