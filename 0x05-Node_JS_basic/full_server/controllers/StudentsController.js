36286fd7d511286c4920a5947482643c2e513938
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 18:49:03 +0100

    fixed babel-node's import/export

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,19 +1,22 @@
-const { readDatabase } = require('../utils')
+import readDatabase from "../utils.js";
+const path = process.argv[2];
 
 class StudentsController {
   static getAllStudents(request, response) {
     this.response = response;
-    this.data = readDatabase('../../database.csv')
+    readDatabase(path)
+      .then((data) => {
+        const responses = ['This is the list of our students'];
+        Object.keys(data).forEach((key) => {
+            responses.push(`Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`);
+        });
+        this.response.statusCode = 200;
+        this.response.send(`${responses.join('\n')}`);
+      })
       .catch((error) => {
         this.response.statusCode = 500;
-        throw new Error('Cannot load the database');
+        this.response.send('Cannot load the database');
       });
-    const responses = ['This is the list of our students'];
-    Object.keys(this.data).forEach((key) => {
-      responses.push(`Number of students in ${key}: ${this.data[key].length}. List: ${this.data[key].join(', ')}`);
-    });
-    this.response.statusCode = 200;
-    this.response.send(`${responses.join('\n')}`);
   }
 
   static getAllStudentsByMajor(request, response) {
@@ -23,17 +26,20 @@ class StudentsController {
     const major = this.request.params.major;
 
     if (major === 'CS' || major === 'SWE') {
-      readDatabase('../../database.csv')
+      readDatabase(path)
         .then((data) => {
           this.response.statusCode = 200;
           this.response.send(`List: ${data[major].join(', ')}`);
         })
         .catch((error) => {
           this.response.statusCode = 500;
-          throw new Error('Cannot load the database');
+          this.response.send('Cannot load the database');
         });
+        return;
     }
     this.response.statusCode = 500;
     this.response.send('Major parameter must be CS or SWE');
   }
 }
+
+export default StudentsController;
