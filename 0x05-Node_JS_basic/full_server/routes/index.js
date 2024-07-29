79de76610050fe6f7b78bc0f6e6eb0b305da2633
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 17:27:58 +0100

    index

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,7 +1,10 @@
 const { AppController } = require('../controllers/AppController');
 const { StudentsController } = require('../controllers/StudentsController');
+import { response } from '../../6-http_express';
 import app from '../server';
 
 app.get('/', (req, res) => {
-  AppController.
+  AppController.getHomepage(req, response);
 });
+
+app.get
