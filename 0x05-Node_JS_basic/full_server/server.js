212617c6a3d95608ff775f4edff511f26d37eeae
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 17:40:42 +0100

    app

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -4,5 +4,7 @@ const app = express();
 const port = 1245;
 
 app.listen(port, () => {
-  console.log('Server is running on http://127.0.0.1:1245')
+  console.log('Server is running on http://127.0.0.1:1245');
 });
+
+export default app;
