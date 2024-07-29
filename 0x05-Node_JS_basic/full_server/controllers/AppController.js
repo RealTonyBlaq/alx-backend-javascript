0a847acadcf7017f6b8b6b88b8d10bedfca9d007
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 22:00:30 +0100

    fix

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,9 +1,6 @@
 class AppController {
   static getHomepage(request, response) {
-    this.response = response;
-    this.request = request;
-    this.response.statusCode = 200;
-    this.response.send('Hello Holberton School!');
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
