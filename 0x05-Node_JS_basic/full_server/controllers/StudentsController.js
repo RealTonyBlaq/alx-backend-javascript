33930017a2d700e0ea1ac7dc63c9e4066705de6c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 21:47:55 +0100

    fix eslint

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,44 +1,38 @@
-import readDatabase from "../utils.js";
+/* eslint-disable import/extensions */
+import readDatabase from '../utils.js';
+
 const path = process.argv[2];
 
 class StudentsController {
   static getAllStudents(request, response) {
-    this.response = response;
     readDatabase(path)
       .then((data) => {
         const responses = ['This is the list of our students'];
         Object.keys(data).forEach((key) => {
-            responses.push(`Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`);
+          data[key].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+          responses.push(`Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`);
         });
-        this.response.statusCode = 200;
-        this.response.send(`${responses.join('\n')}`);
+        response.status(200).send(`${responses.join('\n')}`);
       })
-      .catch((error) => {
-        this.response.statusCode = 500;
-        this.response.send('Cannot load the database');
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
       });
   }
 
   static getAllStudentsByMajor(request, response) {
-    this.response = response;
-    this.request = request;
-
-    const major = this.request.params.major;
+    const { major } = request.params;
 
     if (major === 'CS' || major === 'SWE') {
       readDatabase(path)
         .then((data) => {
-          this.response.statusCode = 200;
-          this.response.send(`List: ${data[major].join(', ')}`);
+          response.status(200).send(`List: ${data[major].join(', ')}`);
         })
-        .catch((error) => {
-          this.response.statusCode = 500;
-          this.response.send('Cannot load the database');
+        .catch(() => {
+          response.status(500).send('Cannot load the database');
         });
-        return;
+      return;
     }
-    this.response.statusCode = 500;
-    this.response.send('Major parameter must be CS or SWE');
+    response.status(500).send('Major parameter must be CS or SWE');
   }
 }
 
