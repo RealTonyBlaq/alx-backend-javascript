5b23134fa7480324a48b353274c0c2d679da704d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 17:49:10 +0100

    fix

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,15 +1,19 @@
-import app from '../server';
+const express = require('express');
 const { AppController } = require('../controllers/AppController');
 const { StudentsController } = require('../controllers/StudentsController');
 
-app.get('/', (req, res) => {
+const router = express.Router()
+
+router.get('/', (req, res) => {
   AppController.getHomepage(req, res);
 });
 
-app.get('/students', (req, res) => {
+router.get('/students', (req, res) => {
   StudentsController.getAllStudents(req, res);
 });
 
-app.get('/students/:major', (req, res) => {
+router.get('/students/:major', (req, res) => {
   StudentsController.getAllStudentsByMajor(req, res);
 });
+
+module.exports = router;
