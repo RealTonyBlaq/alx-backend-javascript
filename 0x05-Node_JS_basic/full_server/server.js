f772fa86a47aa1e6f6100dc375bf2094d859098c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-29 17:48:48 +0100

    app

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -4,7 +4,7 @@ const { router } = require('./routes/index')
 const app = express();
 const port = 1245;
 
-app.use()
+app.use(router);
 
 app.listen(port, () => {
   console.log('Server is running on http://127.0.0.1:1245');
